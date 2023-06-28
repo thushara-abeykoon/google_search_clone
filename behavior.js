51702ec5326b38388f3bf574bb86695a39d00ded
Thushara Abeykoon <thushara.abeykoon99@gmail.com>
2023-06-28 23:34:10 +0530

    major changes to behavior.js

--- behavior.js
+++ behavior.js
@@ -3,25 +3,15 @@ var crossIcon = document.getElementById("cross");
 var textInput = document.getElementById("textInput");
 var slashIcon = document.getElementById("slash");
 
+textInput.onfocus = ()=>{textBox.style.boxShadow = "0px 0px 5px 2px rgb(224, 224, 224)";}
+
+textInput.onblur = ()=>{textBox.attributeStyleMap.clear();}
+
 crossIcon.onclick = function () {
     textInput.value = "";
     crossIcon.style.display = "none";
     slashIcon.style.display = "none";
     textInput.focus();
-}
-
-textInput.onfocus = function () {
-    textBox.style.boxShadow = "0px 0px 5px 2px rgb(224, 224, 224)";
-}
-
-textInput.onblur = function () {
-    textBox.style.boxShadow = "none";
-    textBox.onmouseover = function () {
-        textBox.style.boxShadow = "0px 0px 5px 2px rgb(224, 224, 224)";
-    }
-    textBox.onmouseleave = function(){
-        textBox.style.boxShadow = "none";
-    }
 }
 
 textInput.oninput = function(){
